4286af8e3d2a3e23b8714678dec006e62d5c3f05
Sreimer78 <sreimer@hawkiit.edu>
2020-10-25 18:35:07 -0500

    Updated site.js file and added index.html file to display the game board
    Updated site.js file and added index.html file to display the game board

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,5 +1,10 @@
-var socket = io.connect('/');
+//var socket = io.connect('/');
 
-socket.on('message', function(data) {
-  console.log('Message received: ' + data);
+//socket.on('message', function(data) {
+//  console.log('Message received: ' + data);
+//});
+
+var namespace = io.connect('/' + NAMESPACE);
+namespace.on('message', function(data) {
+  console.log('Message recieved: ' + data);
 });
