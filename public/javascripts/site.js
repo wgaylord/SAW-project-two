a92793c7b3453117b725e9bf9e9111c572b53d17
Sreimer78 <sreimer@hawkiit.edu>
2020-10-25 19:02:53 -0500

    Updated index.html, site.js, and style.css files to start the process of creating a checkerboard
    Updated index.html, site.js, and style.css files to start the process of creating a checkerboard

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -8,3 +8,17 @@ var namespace = io.connect('/' + NAMESPACE);
 namespace.on('message', function(data) {
   console.log('Message recieved: ' + data);
 });
+
+// Placing the red + black pieces on the checkerboard
+// This is only a beta test function for now
+var board = {
+  red: ['A1','A3','B2','B4'],
+  black: ['C2','C4','D1', 'D3'],
+}
+
+// This is how we keep track of which checker pieces have been removed from the checkerboard
+// var score = {
+// 
+// }
+
+board.freeze();
