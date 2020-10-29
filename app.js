357ce82458e5c49b20ed8df0c7ebc05beae344fd
Abdulaziz Aldossari <aaldossari@hawk.iit.edu>
2020-10-29 15:45:32 -0500

    Handle signaling events

--- app.js
+++ app.js
@@ -32,7 +32,18 @@ const namespaces = socket.of(/^\/[a-z]{3}-[a-z]{4}-[a-z]{3}$/);
 
 namespaces.on('connection', function(io) {
   const namespace = io.nsp;
-  namespace.emit('message', 'Successfully connected on namespace: ${namespace.name}');
+  io.emit('message', 'Successfully connected on namespace: ${namespace.name}');
+  io.on('calling', function() {
+    io.broadcast.emit('calling');
+});
+// Handle signaling events and their destructured object data
+  io.on('signal', function({ description, candidate}) {
+    console.log(`Received a signal from ${io.id}`);
+    console.log({description, candidate});
+    // We want to broadcast the received signal so that the sending
+    // side does not receive its own description or candidate
+    io.broadcast.emit('signal', { description, candidate });
+  });
 });
 
 // catch 404 and forward to error handler
