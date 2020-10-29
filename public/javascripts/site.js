30ed96fdfde98bf1abb087574021ff2137abc850
Abdulaziz Aldossari <aaldossari@hawk.iit.edu>
2020-10-29 16:34:17 -0500

    Add peer connection code

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -57,11 +57,13 @@ function startCall() {
   clientIs.polite = true;
   sc.emit('calling');
   startStream();
+  negotiateConnection();
 }
 
 // Handle the 'calling' event on the receiving peer (the callee)
 sc.on('calling', function() {
   console.log('This is the receiving side of the connection...');
+  negotiateConnection();
   callButton.innerText = "Answer Call";
   callButton.id = "answer-button";
   callButton.removeEventListener('click', startCall);
@@ -70,6 +72,86 @@ sc.on('calling', function() {
     startStream();
   });
 });
+
+// Setting up the peer connection.
+async function negotiateConnection() {
+  pc.onnegotiationneeded = async function() {
+    try {
+      console.log('Making an offer...');
+      clientIs.makingOffer = true;
+      try {
+        // Very latest browsers are totally cool with an
+        // argument-less call to setLocalDescription:
+        await pc.setLocalDescription();
+      } catch(error) {
+        // Older (and not even all that old) browsers
+        // are NOT cool. So because we're making an
+        // offer, we need to prepare an offer:
+        var offer = await pc.createOffer();
+        await pc.setLocalDescription(new RTCSessionDescription(offer));
+      } finally {
+        sc.emit('signal', { description: pc.localDescription });
+      }
+    } catch(error) {
+        console.error(error);
+    } finally {
+        clientIs.makingOffer = false;
+    }
+  }
+}
+
+sc.on('signal', async function({ candidate, description }) {
+  try {
+    if (description) {
+      console.log('Received a decription...');
+      var offerCollision  = (description.type == 'offer') &&
+                            (clientIs.makingOffer || pc.signalingState != 'stable')
+      clientIs.ignoringOffer = !clientIs.polite && offerCollision;
+
+      if (clientIs.ignoringOffer) {
+        return; // Just leave if we're ignoring offers
+      }
+
+      // Set the remote description...
+      await pc.setRemoteDescription(description);
+
+      // ...if it's an offer, we need to answer it:
+      if (description.type == 'offer') {
+        console.log('Specifically, an offer description...');
+          try {
+            // Very latest browsers are totally cool with an
+            // argument-less call to setLocalDescription:
+            await pc.setLocalDescription();
+          } catch(error) {
+            // Older (and not even all that old) browsers
+            // are NOT cool. So because we're handling an
+            // offer, we need to prepare an answer:
+            var answer = await pc.createAnswer();
+            await pc.setLocalDescription(new RTCSessionDescription(answer));
+          } finally {
+            sc.emit('signal', { description: pc.localDescription });
+          }
+      }
+
+    } else if (candidate) {
+        console.log('Received a candidate:');
+        console.log(candidate);
+        // Save Safari and other browsers that can't handle an
+        // empty string for the `candidate.candidate` value:
+        if (candidate.candidate.length > 1) {
+          await pc.addIceCandidate(candidate);
+        }
+    }
+  } catch(error) {
+    console.error(error);
+  }
+
+});
+
+// Logic to send candidate
+pc.onicecandidate = function({candidate}) {
+  sc.emit('signal', { candidate: candidate });
+}
 
 // Placing the red + black pieces on the checkerboard
 // This is only a beta test function for now
