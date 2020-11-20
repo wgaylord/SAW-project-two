888528ab16b8fffc172c617525ce119961a9c71e
William <chibill110@gmail.com>
2020-11-20 16:25:37 -0600

    Removed checkers from the app.js

--- app.js
+++ app.js
@@ -44,9 +44,7 @@ namespaces.on('connection', function(socket) {
     // side does not receive its own description or candidate
     socket.broadcast.emit('signal', { description, candidate });
   });
-  socket.on('checkers', function(data) {
-    socket.broadcast.emit('checkers',data);
-});
+ 
 
 });
 
